be841dd84040ad696351fbdfa79125b76ee91a71
souleymane <souleymanedialloo@yahoo.fr>
2022-10-02 19:29:38 +0200

    add postgresql

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_auto',
+        'USER': 'souleymane',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
